1d985da8429f9004c4976f830b299849e1eb2fa5
swon95 <shimtkddnjs13@gmail.com>
2024-01-17 02:09:38 +0900

    feat: extensions - cap

--- contract/contracts/SJKToken.sol
+++ contract/contracts/SJKToken.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract SJKToken is ERC20 {
-    constructor() ERC20("SJKToken", "SJK") {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+contract SJKToken is ERC20Capped {
+    // 1 억개로 제한
+    constructor(uint256 cap) ERC20("SJKToken", "SJK") ERC20Capped(cap * (10 ** decimals())){
+        // 7 천만개 발행
+        _mint(msg.sender, 70000000 * (10 ** uint256(decimals())));
     }
 }
